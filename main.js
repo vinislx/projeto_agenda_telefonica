6437884933e3ff02ee0031ec8cbfaa086587a9cf
vinislx <vini91911@gmail.com>
2024-11-20 20:09:52 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('form-telefone');
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeAgenda = document.getElementById('nome-agenda');
+    const inputNumeroTelefone = document.getElementById('numero-telefone');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAgenda.value}</td>`;
+    linha += `<td>${inputNumeroTelefone.value}</td>`;
+    linha += '<tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    alert(`O número foi agendado ${inputNomeAgenda.value} ${inputNumeroTelefone.value} `);
+
+});
